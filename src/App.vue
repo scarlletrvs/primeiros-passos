ab402489a4068d000c158dcc1b562e809cd92e2c
scarlletrvs <scarlletrvs34@gmail.com>
2023-09-25 19:53:16 -0300

    hello world

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/beyond-logo.png" style="width: 50%;">
+  <HelloWorld msg="Welcome to my Beyond App" style=" font-size: 20px"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:#F19F05;
+  margin-top: 60px;
+  
+ 
+}
+body{
+  background-color:#e1dddd;
+}
+</style>
